63de89d70f72e0483ba725b30dedac44804db7bc
Herzo <Herzo@TACHISBOOK5PRO>
2026-07-15 22:12:12 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KnownFirst
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KnownFirst
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
